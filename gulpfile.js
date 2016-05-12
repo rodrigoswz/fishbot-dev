4532eb2c0eb1bd20472ed87945f707bfb7f1e87b
picles <eduselcip@gmail.com>
2016-05-12 23:51:38 +0000

    arrumando painel avancado

--- gulpfile.js
+++ gulpfile.js
@@ -197,7 +197,7 @@ gulp.task('clean', function() {
 // Watch files for changes & reload
 gulp.task('serve', ['styles'], function() {
   browserSync({
-    port: 5000,
+    port: 8081,
     notify: false,
     logPrefix: 'PSK',
     snippetOptions: {
